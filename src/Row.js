73ed5968ef8992c5e916e5125503a22d44676d1a
washtakzo <washtakzo@gmail.com>
2022-10-20 12:38:59 +0200

    row ok

--- src/Row.js
+++ src/Row.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
 
+const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original";
+
 const Row = ({ title, fetchUrl, isLargeRow = false }) => {
   const [movies, setMovies] = useState([]);
 
@@ -20,16 +22,21 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
     <section className="row">
       <h2 className="row__title">{title}</h2>
       <div className="row__images">
-        {movies?.map((movie) => (
-          <img
-            className={`row__image ${isLargeRow && "row__image__landscape"}`}
-            key={movie.id}
-            src={`https://image.tmdb.org/t/p/original${
-              isLargeRow ? movie?.backdrop_path : movie?.poster_path
-            }`}
-            alt="movie card"
-          />
-        ))}
+        {movies?.map((movie) => {
+          //if no image link available return without trying to make an image
+          if (isLargeRow && !movie.poster_path) return;
+          if (!isLargeRow && !movie.backdrop_path) return;
+          return (
+            <img
+              className={`row__image ${isLargeRow && "row__image__large"}`}
+              key={movie.id}
+              src={`${BASE_IMAGE_URL}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          );
+        })}
       </div>
     </section>
   );
