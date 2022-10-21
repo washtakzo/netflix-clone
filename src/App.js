916d014d80d8958c7fd90795e04aae1dc0831900
washtakzo <washtakzo@gmail.com>
2022-10-21 18:50:38 +0200

    css done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
+const user = null;
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: user != null ? <HomeScreen /> : <LoginScreen />,
+  },
+  {
+    path: "login",
+    element: <LoginScreen />,
+  },
+]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+<React.StrictMode>
+  <RouterProvider router={router} />
+</React.StrictMode>;
+// );
 function App() {
   return (
     <div className="App">
-      <HomeScreen />
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
     </div>
   );
 }
