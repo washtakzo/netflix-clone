5f08bc6d24ad76619b79d0ccb18196c995ff483b
washtakzo <washtakzo@gmail.com>
2022-10-24 11:48:09 +0200

    profil page in progress + logout button ok

---
+++ src/screens/ProfilScreen.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./ProfilScreen.css";
+import Nav from "../Nav";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+
+const ProfilScreen = () => {
+  const user = useSelector(selectUser);
+  return (
+    <div className="profilScreen">
+      <Nav />
+      <div className="profilScreen__body">
+        <h1>Edit Profile</h1>
+        <div className="profilScreen__body__detail">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="avatar logo"
+          />
+          <div className="profilScreen__body__info">
+            <h2>{user?.email}</h2>
+            <h3>Plans (Current Plan : premium)</h3>
+            <button
+              className="profilScreen__signout"
+              onClick={() => signOut(auth)}
+            >
+              Sign Out
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilScreen;
