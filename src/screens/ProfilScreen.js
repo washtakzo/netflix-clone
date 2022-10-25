288a00b49679558e6fa269d295c128423b1f456b
washtakzo <washtakzo@gmail.com>
2022-10-25 19:27:21 +0200

    plan subscription components

--- src/screens/ProfilScreen.js
+++ src/screens/ProfilScreen.js
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import PlanSubsription from "../components/PlanSubsription";
 
 const ProfilScreen = () => {
   const user = useSelector(selectUser);
@@ -21,6 +22,16 @@ const ProfilScreen = () => {
           <div className="profilScreen__body__info">
             <h2>{user?.email}</h2>
             <h3>Plans (Current Plan : premium)</h3>
+            <h4>Renewal date : 04/03/2022</h4>
+            <div className="profilScreen__plans">
+              <PlanSubsription title="Netflix Basic" option="720p" />
+              <PlanSubsription
+                title="Netflix Standard"
+                option="1080p"
+                isCurrentPlan={true}
+              />
+              <PlanSubsription title="Netflix Premium" option="4k+HDR" />
+            </div>
             <button
               className="profilScreen__signout"
               onClick={() => signOut(auth)}
